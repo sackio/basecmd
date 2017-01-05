30ff4528681febf8acebf559ad79cc353cd19108
Ben Sack <ben@sack.io>
2017-01-05 11:33:41 -0500

    latest

--- scripts/daemon.js
+++ scripts/daemon.js
@@ -8,7 +8,7 @@ var Forever = require('forever-monitor')
   , _ = require('underscore')
   , OS = require('os')
   , O = new Optionall({
-                       '__dirname': process.env.dirname || Path.resolve(module.filename + '/../..')
+                       '__dirname': process.env.rootdir || Path.resolve(module.filename + '/../..')
                      , 'file_priority': [
                          'package.json'
                        , 'assets.json'
